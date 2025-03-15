ccfea3f9bae491e5203ecaaedebe01f07a73327f
Dhirendra Kumar Singh <159696018+ElephantRifle@users.noreply.github.com>
2025-03-15 14:26:35 +0530

    Update Insertion_Sort.cpp

--- 2.  Algorithms/Comparison Based Sorting/Insertion_Sort.cpp
+++ 2.  Algorithms/Comparison Based Sorting/Insertion_Sort.cpp
@@ -37,10 +37,14 @@ int main(){
 }
 
 
-// This code will work but it dosen't implement the logic correctly 
+// This code will work but it dosen't implement the logic correctly .
 // In this we are swapping the number instead we have to shift the values to their correct position
 /* Why shifting ? bec when we require to put a value at its correct place in sorted array from unsorted
-*/ we have to perform SHIFTING NOT Swapping
+*/ we have to perform SHIFTING NOT Swapping its more efficient.
+
+// Issue: i-- inside the loop changes the for loop’s control, which can cause skipped elements or extra iterations.
+// Risk: Might work in some cases, but not always reliable.
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -54,10 +58,38 @@ int main() {
             int temp = arr[i];
             arr[i] = arr[prev];
             arr[prev] = temp;
-            i--;
+            i--;             // Here i get its value changed 
             prev--;
         }
         
+    }
+    for(auto temp : arr){
+        cout<<temp<<" ";
+    }
+
+    return 0;
+}
+
+
+// This Approach i a bit better than the above as it dosen't change 'i'
+// Fixes the issue: Keeps i untouched, making the for loop work as expected.
+// Result: More consistent and reliable than the first version.
+#include <iostream>
+using std::cout;
+int main() {
+    int arr[]{10,9,8,7,6,5,4,3,2,1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i = 1;i < size;++i){
+        int a = i;
+        int prev = i - 1;
+        while( prev >= 0 && arr[a] < arr[prev]){
+            int temp = arr[a];
+            arr[a] = arr[prev];
+            arr[prev] = temp;
+            a--;             // Moves only 'a' , not 'i'
+            prev--;
+        }
     }
     for(auto temp : arr){
         cout<<temp<<" ";
