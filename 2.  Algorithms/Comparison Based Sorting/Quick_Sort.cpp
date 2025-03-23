f05966d473050a9446436eba3ada857d6dfff22e
ElephantRifle <dhirendrakumar300200@gmail.com>
2025-03-24 00:58:47 +0530

    test

--- 2.  Algorithms/Comparison Based Sorting/Quick_Sort.cpp
+++ 2.  Algorithms/Comparison Based Sorting/Quick_Sort.cpp
@@ -11,7 +11,10 @@ Space Complexity: 𝑂(log𝑛)  (average case for recursion)
 
 //This is the code to put a pivot at its correct place
 
+//#define code1
+#define values 
 
+#ifdef code1
 
 #include <iostream>
 #include<vector>
@@ -55,3 +58,59 @@ int main() {
 
     return 0;
 }
+
+#endif
+
+
+#ifdef values
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int quick(vector<int>&arr,int low,int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+        while(arr[i] <= pivot && i <= high-1){
+            i++;
+        }
+        while(arr[j] > pivot && j >= low+1){
+            j--;
+        }
+        if(i < j){
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[low] , arr[j]);
+    cout<<low<<" "<<high<<endl;
+    for(auto temp: arr){
+        cout<<temp<<" ";
+    }
+    cout<<'\n';
+    cout<<"british: "<<j<<endl;
+    return j;
+}
+
+void quick_sort(vector<int>&arr,int low,int high){
+   if(low < high){
+       int british = quick(arr,low,high);
+       quick_sort(arr,low,british-1);
+       quick_sort(arr,british+1,high);
+   } 
+}
+int main() {
+    vector<int>arr{4,6,2,5,7,9,1,3};
+    int size = arr.size();
+    
+    quick_sort(arr,0,size-1);
+    
+    // for(auto temp : arr){
+    //     cout<<temp<<" ";
+    // }
+
+    return 0;
+}
+
+#endif
