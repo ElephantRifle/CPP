e47c072accb1b7d5fb805f0b0d0af213b92bc45c
ElephantRifle <dhirendrakumar300200@gmail.com>
2025-03-23 18:19:39 +0530

    quick

--- 2.  Algorithms/Comparison Based Sorting/Quick_Sort.cpp
+++ 2.  Algorithms/Comparison Based Sorting/Quick_Sort.cpp
@@ -10,4 +10,48 @@ Space Complexity: 𝑂(log𝑛)  (average case for recursion)
 */
 
 //This is the code to put a pivot at its correct place
-//Hello there
+
+
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int quick(vector<int>&arr,int low,int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+        while(arr[i] <= pivot && i <= high-1){
+            i++;
+        }
+        while(arr[j] > pivot && j >= low+1){
+            j--;
+        }
+        if(i < j){
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[low] , arr[j]);
+    return j;
+}
+
+void quick_sort(vector<int>&arr,int low,int high){
+   if(low < high){
+       int british = quick(arr,low,high);
+       quick_sort(arr,low,british-1);
+       quick_sort(arr,british+1,high);
+   } 
+}
+int main() {
+    vector<int>arr{4,6,2,5,7,9,1,3,4};
+    int size = arr.size();
+    
+    quick_sort(arr,0,size-1);
+    
+    for(auto temp : arr){
+        cout<<temp<<" ";
+    }
+
+    return 0;
+}
