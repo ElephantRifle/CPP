adca9b15e16a1788232643f2305cb2da90c4eb52
ElephantRifle <dhirendrakumar300200@gmail.com>
2026-02-20 21:08:25 +0530

    Prac

--- 6. Alorithmic Techniques/tst.cpp
+++ 6. Alorithmic Techniques/tst.cpp
@@ -4,16 +4,25 @@
 
 using namespace std;
 int main(){
-    int arr[]{1,1,0,1,1,1,0,1,1,1,1,0};
+    int arr[]{0,1,0,1,1,1};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     int left,right{};
     int maxlen{};
+    int zero_count{};
+    int k{1};
 
     while(right < size){
         if(arr[right] == 0){
-            left = right + 1;
+            zero_Count++;
         }
+        while(zero_count > 1){
+            if(arr[left] == 0){
+                zero_count--;
+            }
+            left++;
+        }
+        
         maxlen = max(maxlen,right - left + 1);
         right++;
     }
